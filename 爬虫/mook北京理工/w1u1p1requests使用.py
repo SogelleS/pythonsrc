7f39cca5d699895be04312bc3a6b0291cb77bc77
[SogelleS] <xxaabbc@outlook.com>
2020-04-05 13:17:49 +0800

    renew

--- 爬虫/mook北京理工/w1u1p1requests使用.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r = requests.get('https://www.baidu.com')
-print(r.apparent_encoding)
-print(r.text)
-r.encoding = 'utf-8'
-# 如果httphader没有编码形式，默认编码为iso-8859-1
-# apparent_encoding是根据分析获取的网页编码
-print(r.text)
